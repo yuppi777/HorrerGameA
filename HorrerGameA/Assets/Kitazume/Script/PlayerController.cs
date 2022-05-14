df8013cc20189a8abf4f0e6cab80a706601f4da2
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2022-05-15 02:22:52 +0900

    コミット

---
+++ HorrerGameA/Assets/Kitazume/Script/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+     private Rigidbody rB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rB.AddForce(x, 0, z);
+    }
+    void Update()
+    {
+        
+    }
+}
