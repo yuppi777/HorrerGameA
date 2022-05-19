0515356653e198cf76c08a1a9842a80185192469
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2022-05-19 14:13:48 +0900

    プレイヤースクリプトの変更

--- HorrerGameA/Assets/Kitazume/Script/PlayerController.cs
+++ HorrerGameA/Assets/Kitazume/Script/PlayerController.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 10.0f;
-     private Rigidbody rB;
+    private CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
 
-    // Start is called before the first frame update
+    public float _mainSpeed = 3.0f;
+    public float mainJampSpeed = 8.0f;
+    public float gravity = 20.0f;
     void Start()
     {
-        rB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+    }
+        void Update()
+        {
+            if (controller.isGrounded)
+            {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection = moveDirection * _mainSpeed;
+                if (Input.GetButton("Jump"))
+                {
+                    moveDirection.y = mainJampSpeed;
+                }
+            }
+            moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+            controller.Move(moveDirection * Time.deltaTime);
+        }
     }
 
-    // Update is called once per frame
-
-    private void FixedUpdate()
-    {
-        float x = Input.GetAxis("Horizontal") * speed;
-        float z = Input.GetAxis("Vertical") * speed;
-        rB.AddForce(x, 0, z);
-    }
-    void Update()
-    {
-        
-    }
-}
