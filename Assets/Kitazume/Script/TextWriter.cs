27f7767cea602f6f0b80d1705b45cc0cb7e9896d
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2022-08-10 14:43:41 +0900

    会話システムの完成

---
+++ Assets/Kitazume/Script/TextWriter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextWriter : MonoBehaviour
+{
+    public UIText uitext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Cotest");
+    }
+
+    // クリック待ちのコルーチン
+    IEnumerator Skip()
+    {
+        while (uitext.playing) yield return 0;
+        while (!uitext.IsClicked()) yield return 0;
+    }
+
+    // 文章を表示させるコルーチン
+    IEnumerator Cotest()
+    {
+        uitext.DrawText("ナレーションだったらこのまま書けばOK");
+        yield return StartCoroutine("Skip");
+
+        uitext.DrawText("やみこ", "人が話すのならこんな感じ");
+        yield return StartCoroutine("Skip");
+
+    }
+}
---
+++ Assets/Kitazume/Script/UIText.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// UIを使っているのでこれを記入しましょう
+using UnityEngine.UI;
+public class UIText : MonoBehaviour
+{
+    // nameText:喋っている人の名前
+    // talkText:喋っている内容やナレーション
+    public Text nameText;
+    public Text talkText;
+
+    public bool playing = false;
+    public float textSpeed = 0.1f;
+
+    void Start() { }
+
+    // クリックで次のページを表示させるための関数
+    public bool IsClicked()
+    {
+        if (Input.GetMouseButtonDown(0)) return true;
+        return false;
+    }
+
+    // ナレーション用のテキストを生成する関数
+    public void DrawText(string text)
+    {
+        nameText.text = "";
+        StartCoroutine("CoDrawText", text);
+    }
+    // 通常会話用のテキストを生成する関数
+    public void DrawText(string name, string text)
+    {
+        nameText.text = name + "\n「";
+        StartCoroutine("CoDrawText", text + "」");
+    }
+
+    // テキストがヌルヌル出てくるためのコルーチン
+    IEnumerator CoDrawText(string text)
+    {
+        playing = true;
+        float time = 0;
+        while (true)
+        {
+            yield return 0;
+            time += Time.deltaTime;
+
+            // クリックされると一気に表示
+            if (IsClicked()) break;
+
+            int len = Mathf.FloorToInt(time / textSpeed);
+            if (len > text.Length) break;
+            talkText.text = text.Substring(0, len);
+        }
+        talkText.text = text;
+        yield return 0;
+        playing = false;
+    }
+}
