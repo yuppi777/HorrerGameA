27ad497c99e57350665f78cd8a12c42061fe44a5
abe0929 <93189773+abe0929@users.noreply.github.com>
2022-09-01 00:27:54 +0900

    ああ

---
+++ Assets/abe/Script/Fadeout.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fadeout : MonoBehaviour
+{
+
+
+    public GameObject Panelfade;   //フェードパネルの取得
+
+    Image fadealpha;               //フェードパネルのイメージ取得変数
+
+    private float alpha;           //パネルのalpha値取得変数
+
+    private bool fadeout;          //フェードアウトのフラグ変数
+
+    public int SceneNo;            //シーンの移動先ナンバー取得変数
+
+
+    // Use this for initialization
+    void Start()
+    {
+        fadealpha = Panelfade.GetComponent<Image>(); //パネルのイメージ取得
+        alpha = fadealpha.color.a;                 //パネルのalpha値を取得
+        fadeout = true;                             //シーン読み込み時にフェードインさせる
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (fadeout == true)
+        {
+            FadeOut();
+        }
+    }
+
+    void FadeOut()
+    {
+        alpha += 0.001f;
+        fadealpha.color = new Color(0, 0, 0, alpha);
+        if (alpha >= 1)
+        {
+            fadeout = false;
+        }
+    }
+
+
+}
---
+++ Assets/abe/Script/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public AudioClip sound1;
+    public AudioClip sound2;
+    AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(sound1);
+        audioSource.PlayOneShot(sound2);
+
+
+    }
+}
