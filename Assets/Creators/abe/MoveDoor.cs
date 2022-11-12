1a4e1d4e32f6c7ebbf2f679b2d5329288cf516b5
abe0929 <93189773+abe0929@users.noreply.github.com>
2022-11-12 15:20:50 +0900

    旧校舎　

--- Assets/Creators/abe/MoveDoor.cs
+++ Assets/Creators/abe/MoveDoor.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MoveDoor : MonoBehaviour
 {
+    private bool _neerDoor;
+    private bool _OpenDoor = true;
     public static bool _MasterKey = false;
     [SerializeField] GameObject _playerObj;
     [SerializeField]float time = 0;
-   
-
-    private void OnTriggerStay(Collider other)
+    private void Update()
     {
-        if (other.gameObject == _playerObj)
+        if (Input.GetKey(KeyCode.U))//デバック用
         {
+            _MasterKey = true;
+        }
+        if (_neerDoor == true && _OpenDoor ==true) {
             if (Input.GetKey(KeyCode.E))
             {
                 if (_MasterKey == false)
@@ -22,19 +26,34 @@ public class MoveDoor : MonoBehaviour
                 }
                 else if (_MasterKey == true)
                 {
-                    for (float i = 0; i < time; i++)
-                    {
-                        Transform trans = transform;
-                        transform.position = trans.position;
-                        trans.position += trans.TransformDirection(Vector3.right) * 0.01f * Time.deltaTime;
-                    }
+                    /* for (float i = 0; i <= time; i++)
+                     {
+                         Transform trans = transform;
+                         transform.position = trans.position;
+                         trans.position += trans.TransformDirection(Vector3.right) * 0.01f * Time.deltaTime;
+                     }*/
+                    SceneManager.LoadScene("SecondMap");
+
                 }
-            }
-            if (Input.GetKey(KeyCode.U))//デバック用
-            {
-                _MasterKey = true;
+
+               
             }
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _playerObj)
+        {
+            _neerDoor = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == _playerObj)
+        {
+            _neerDoor = false;
+        }
+    }
+
 
 }
