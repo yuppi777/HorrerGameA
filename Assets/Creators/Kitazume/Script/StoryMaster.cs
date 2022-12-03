d36335590f27e52b9c2e9aa9039187af6ef882b0
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2022-12-03 15:21:15 +0900

    ストーリーのスクリプタブルオブジェクト作成

---
+++ Assets/Creators/Kitazume/Script/StoryMaster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[CreateAssetMenu(menuName = "ScriptableObject/CreateAsset")]
+
+public class StoryMaster : ScriptableObject
+{
+    public List<StoryMasterRecord> sheet;
+
+    //public enum JobPost
+    //{
+    //    Programer,
+    //    Farmers,
+    //    Idol,
+    //}
+
+
+    [Serializable]
+    public class StoryMasterRecord
+    {
+        public string storyName;
+        public Sprite sprite;
+        public TextAsset CsvFile;
+        //public StoryMaster.JobPost Post;
+
+    }
+}
