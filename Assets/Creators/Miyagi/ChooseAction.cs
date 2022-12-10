12c1e8227633f6c4c931c45fced51437671c09ab
miyagirei <ra.mu.umy243@gmail.com>
2022-12-10 14:25:27 +0900

    prefabsFile

---
+++ Assets/Creators/Miyagi/ChooseAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChooseAction : MonoBehaviour
+{
+    enum SelectType
+    {
+        PanelDisplay,
+        MoveDoor,
+    }
+}
