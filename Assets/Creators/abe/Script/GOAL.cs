e6832c9b12f09e53c96cf0b06f52e270ecfd2fe7
abe0929 <93189773+abe0929@users.noreply.github.com>
2023-02-04 14:20:16 +0900

    0204

--- Assets/Creators/abe/Script/GOAL.cs
+++ Assets/Creators/abe/Script/GOAL.cs
@@ -7,7 +7,7 @@ public class GOAL : MonoBehaviour
 {
     public static bool ItemKey = false;
 
-    [SerializeField]Action act;
+    //[SerializeField]Action act;
     private void OnCollisionEnter(Collision collision)
     {
        if(collision.gameObject.tag == "Player")
@@ -23,16 +23,12 @@ public class GOAL : MonoBehaviour
                 Debug.Log("鍵をさがそう");
             }
 
-            if(act.activeKey[4] == true)
+            /*if(act.activeKey[4] == true)
             {
                 SceneManager.LoadScene("GameCliea");
                 Cursor.visible = false;
                 Cursor.lockState = CursorLockMode.Confined;
-            }
-            else
-            {
-
-            }
+            }*/
 
         }
     }
--- Assets/Creators/abe/Script/Item.cs
+++ Assets/Creators/abe/Script/Item.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    [SerializeField] GameObject _getKey;
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("aa");
         if(collision.gameObject.tag == "Player")
         {
             GOAL.ItemKey = true;
+            _getKey.SetActive(true);
             Debug.Log("鍵を入手した");
+            Invoke("falseKetText", 3); 
             this.gameObject.SetActive(false);
+
         }
+    }
+    public void falseKetText()
+    {
+        _getKey.SetActive(false);
     }
 }
